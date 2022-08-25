9a6f20f4144376c18e67a054ab564c3aae9862d8
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-25 15:39:56 +0600

    reduce

---
+++ reduce.js
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3, 4, 5];
+// const total = numbers.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+const total = numbers.reduce((previousValue, currentValue) => {
+    console.log(previousValue, currentValue);
+    return previousValue + currentValue
+}, 0);
+console.log(total);
