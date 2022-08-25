939ac13ecea2534dc133d9f1cd0cd96256dd04b2
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-25 15:29:31 +0600

    find of array

---
+++ find.js
@@ -0,0 +1,6 @@
+const numbers = [20, 11, 34, 45, 16, 10, 45];
+const fives = numbers.find(number => number % 5 == 0);
+console.log(fives);
+
+//filter korle sob gulo dibe
+//find korle sudu 1st element k dibe
