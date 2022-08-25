261cc1ad50a38af644d4321edbe71305963f84b5
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-25 11:38:47 +0600

    chain

---
+++ chain.js
@@ -0,0 +1,10 @@
+const number = [{ id: 01, name: 'Farvez', addess: 'comilla' }]
+console.log(number[0].name);
+
+const data = {
+    count: 5000,
+    data: [{ id: 1, name: 'Abul', job: 'leader' },
+    { id: 1, name: 'Dbul', job: 'leader' }]
+}
+
+console.log(data.data[0].job);
